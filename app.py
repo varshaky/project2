efd6fa5046943094b9ca1321775edd2b4cc522c6
varshaky <varsha4518@gmail.com>
2026-09-21 11:56:18 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app=Flask(__name__)
+@app.route("/")
+def home():
+    return"hello world!this is my first python app"
+if__name__="__main__":
+    app.run(host="0.0.0.0",port=5000)
+    
+

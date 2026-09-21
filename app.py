ff4338b2c9432e30fb6b1034b04979e5b285de01
varshaky <varsha4518@gmail.com>
2026-09-21 12:01:26 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ app=Flask(__name__)
 @app.route("/")
 def home():
     return"hello world!this is my first python app"
-if__name__="__main__":
+if __name__="__main__":
     app.run(host="0.0.0.0",port=5000)
+
     
 
